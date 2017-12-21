620a6b3c0a69e54d0fa3b5a648a47273cf707d71
hani <jhanihashemi@gmail.com>
2017-12-21 17:22:11 +0330

    Add farsi font

--- app/src/main/java/com/hanihashemi/babysleep/App.kt
+++ app/src/main/java/com/hanihashemi/babysleep/App.kt
@@ -1,9 +1,16 @@
 package com.hanihashemi.babysleep
 
 import android.app.Application
+import android.support.v7.app.AppCompatDelegate
 
 /**
  * Created by hani on 12/21/17.
  */
 
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
+}
