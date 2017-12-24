0915da1424a2e56b48220c3e1677f76afb8e1007
hani <jhanihashemi@gmail.com>
2017-12-24 22:36:18 +0330

    Add musicalButton programmatically

--- app/src/main/java/com/hanihashemi/babysleep/MainFragment.kt
+++ app/src/main/java/com/hanihashemi/babysleep/MainFragment.kt
@@ -1,6 +1,12 @@
 package com.hanihashemi.babysleep
 
+import android.view.View
+import android.view.View.inflate
+import android.widget.TextView
 import com.hanihashemi.babysleep.base.BaseFragment
+import com.hanihashemi.babysleep.widget.MusicalButton
+import com.nex3z.flowlayout.FlowLayout
+import kotlinx.android.synthetic.main.main_fragment.*
 
 /**
  * Created by hani on 12/24/17.
@@ -9,6 +15,13 @@ class MainFragment : BaseFragment() {
     override val layoutResource: Int get() = R.layout.main_fragment
 
     override fun customizeUI() {
+        val myLayout = inflate(context, R.layout.nature_section_layout, wrapperLayout)
+        myLayout.findViewById<TextView>(R.id.title).text = "Hani"
+        val musicalButtonLayout = myLayout.findViewById<FlowLayout>(R.id.musicalButtonLayout)
+
+        val musicalButton = MusicalButton(context, null, R.style.MusicalButton)
+
+        musicalButtonLayout.addView(musicalButton)
 
     }
 }
