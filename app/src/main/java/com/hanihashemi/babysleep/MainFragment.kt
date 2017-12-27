f849a18a59a470c75e200af2cc0a4d19425c4080
Hani Hashemi <jhanihashemi@gmail.com>
2017-12-28 00:15:52 +0330

    Using gridview instead of flowlayout

--- app/src/main/java/com/hanihashemi/babysleep/MainFragment.kt
+++ app/src/main/java/com/hanihashemi/babysleep/MainFragment.kt
@@ -2,8 +2,10 @@ package com.hanihashemi.babysleep
 
 import android.view.View
 import android.view.View.inflate
+import android.widget.GridView
 import android.widget.TextView
 import com.hanihashemi.babysleep.base.BaseFragment
+import com.hanihashemi.babysleep.model.Music
 import com.hanihashemi.babysleep.widget.MusicalButton
 import com.nex3z.flowlayout.FlowLayout
 import kotlinx.android.synthetic.main.main_fragment.*
@@ -15,13 +17,24 @@ class MainFragment : BaseFragment() {
     override val layoutResource: Int get() = R.layout.main_fragment
 
     override fun customizeUI() {
+        val musics = mutableListOf<Music>()
+        musics.add(Music(0, "First 0", ""))
+        musics.add(Music(1, "First 1", ""))
+        musics.add(Music(2, "First 2", ""))
+        musics.add(Music(3, "First 3", ""))
+        musics.add(Music(4, "First 4", ""))
+        musics.add(Music(5, "First 5", ""))
+        musics.add(Music(6, "First 6", ""))
+
         val myLayout = inflate(context, R.layout.nature_section_layout, wrapperLayout)
         myLayout.findViewById<TextView>(R.id.title).text = "Hani"
-        val musicalButtonLayout = myLayout.findViewById<FlowLayout>(R.id.musicalButtonLayout)
+//        val musicalButtonLayout = myLayout.findViewById<FlowLayout>(R.id.musicalButtonLayout)
+//        val musicalButton = MusicalButton(context, null, R.style.MusicalButton)
+//        musicalButtonLayout.addView(musicalButton)
 
-        val musicalButton = MusicalButton(context, null, R.style.MusicalButton)
+        val gridView = myLayout.findViewById<GridView>(R.id.gridView)
+        gridView.adapter = MusicalButtonAdapter(context, musics)
 
-        musicalButtonLayout.addView(musicalButton)
 
     }
 }
--- app/src/main/java/com/hanihashemi/babysleep/MusicalButtonAdapter.kt
+++ app/src/main/java/com/hanihashemi/babysleep/MusicalButtonAdapter.kt
@@ -4,21 +4,31 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import com.hanihashemi.babysleep.model.Music
 
 /**
  * Created by hani on 12/24/17.
  */
-class MusicalButtonAdapter(val context: Context, val musics: List<Music>) : BaseAdapter() {
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        
+class MusicalButtonAdapter(private val context: Context, private val musics: List<Music>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val button: Button
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            button = Button(context)
+            button.layoutParams = ViewGroup.LayoutParams(85, 85)
+            button.setPadding(8, 8, 8, 8)
+        } else {
+            button = convertView as Button
+        }
+
+        button.text = musics[position].name
+        return button
     }
 
     override fun getItem(index: Int) = musics[index]
 
-    override fun getItemId(p0: Int): Long {
-
-    }
+    override fun getItemId(index: Int) = musics[index].id
 
     override fun getCount() = musics.size
 }
