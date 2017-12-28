4b2f74fdf6f6f67604721ea3c9c0b151f38eb071
Hani Hashemi <jhanihashemi@gmail.com>
2017-12-28 23:36:27 +0330

    Add timber and listener for each item

--- app/src/main/java/com/hanihashemi/babysleep/MusicalButtonAdapter.kt
+++ app/src/main/java/com/hanihashemi/babysleep/MusicalButtonAdapter.kt
@@ -4,21 +4,22 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.Button
 import com.hanihashemi.babysleep.helper.dpToPx
 import com.hanihashemi.babysleep.model.Music
 import com.hanihashemi.babysleep.widget.MusicalButton
+import timber.log.Timber
 
 /**
  * Created by hani on 12/24/17.
  */
-class MusicalButtonAdapter(private val context: Context, private val musics: List<Music>) : BaseAdapter() {
+class MusicalButtonAdapter(private val context: Context, private val musics: List<Music>) : BaseAdapter(){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val musicalButton: MusicalButton
 
         if (convertView == null) {
             musicalButton = MusicalButton(context, null)
             musicalButton.layoutParams = ViewGroup.LayoutParams(context.dpToPx(70F), context.dpToPx(70F))
+            musicalButton.setOnClickListener{onItemClick(position)}
         } else {
             musicalButton = convertView as MusicalButton
         }
@@ -26,9 +27,13 @@ class MusicalButtonAdapter(private val context: Context, private val musics: Lis
         return musicalButton
     }
 
-    override fun getItem(index: Int) = musics[index]
+    private fun onItemClick(position: Int){
+        Timber.d("position %s clicked", position)
+    }
 
-    override fun getItemId(index: Int) = musics[index].id
+    override fun getItem(position: Int) = musics[position]
+
+    override fun getItemId(position: Int) = musics[position].id
 
     override fun getCount() = musics.size
 }
