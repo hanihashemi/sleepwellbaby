d4ebedb7ae21429aaf0305432bc6f5896d092aca
Hani Hashemi <jhanihashemi@gmail.com>
2018-01-05 14:27:04 +0330

    Add crash handler

--- app/src/main/java/com/hanihashemi/babysleep/App.kt
+++ app/src/main/java/com/hanihashemi/babysleep/App.kt
@@ -2,6 +2,11 @@ package com.hanihashemi.babysleep
 
 import android.app.Application
 import android.support.v7.app.AppCompatDelegate
+import android.util.Log
+import com.hanihashemi.babysleep.helper.FakeCrashLibrary
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 /**
  * Created by hani on 12/21/17.
@@ -12,5 +17,30 @@ class App : Application() {
         super.onCreate()
 
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    /** A tree which logs important information for crash reporting.  */
+    private class CrashReportingTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+
+            FakeCrashLibrary.log(priority, tag, message)
+
+            if (t != null) {
+                if (priority == Log.ERROR) {
+                    FakeCrashLibrary.logError(t)
+                } else if (priority == Log.WARN) {
+                    FakeCrashLibrary.logWarning(t)
+                }
+            }
+        }
     }
 }
