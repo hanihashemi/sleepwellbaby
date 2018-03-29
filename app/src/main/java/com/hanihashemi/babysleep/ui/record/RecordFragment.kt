b22c03ec7bf1f7768f22d7d397f803b62dd35d5e
Hani Hashemi <jhanihashemi@gmail.com>
2018-03-30 02:05:08 +0430

    dummy

--- app/src/main/java/com/hanihashemi/babysleep/ui/record/RecordFragment.kt
+++ app/src/main/java/com/hanihashemi/babysleep/ui/record/RecordFragment.kt
@@ -3,12 +3,24 @@ package com.hanihashemi.babysleep.ui.record
 import com.hanihashemi.babysleep.R
 import com.hanihashemi.babysleep.base.BaseFragment
 
+
 /**
  * Created by Hani on 3/26/18.
  */
-class RecordFragment : BaseFragment(){
+class RecordFragment : BaseFragment() {
     override val layoutResource: Int get() = R.layout.record_fragment
     override fun customizeUI() {
+//        btnCancel.setOnClickListener { activity.finish() }
+//        btnSave.setOnClickListener {}
+    }
 
+    private fun startRecording() {
+//        Toast.makeText(context, "Start Recording", Toast.LENGTH_LONG).show()
+
+        //        var mFileName = activity.dataDir.absolutePath
+//        mFileName += "/audiorecordtest.3gp";
+//        val mediaRecorder = MediaRecorder()
+//        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+//        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS)
     }
 }
