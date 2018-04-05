e88495bb8ded4eb834fccb91fb1d14f8171863b0
Hani Hashemi <jhanihashemi@gmail.com>
2018-04-06 01:05:29 +0430

    Fix Timber

--- app/src/main/java/com/hanihashemi/babysleep/App.kt
+++ app/src/main/java/com/hanihashemi/babysleep/App.kt
@@ -4,15 +4,12 @@ import android.app.Application
 import android.content.Context
 import android.support.v7.app.AppCompatDelegate
 import android.util.Log
-import com.hanihashemi.babysleep.helper.FakeCrashLibrary
 import timber.log.Timber
 import timber.log.Timber.DebugTree
-
 
 /**
  * Created by hani on 12/21/17.
  */
-
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
@@ -30,19 +27,10 @@ class App : Application() {
     /** A tree which logs important information for crash reporting.  */
     private class CrashReportingTree : Timber.Tree() {
         override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG)
                 return
-            }
 
-            FakeCrashLibrary.log(priority, tag, message)
 
-            if (t != null) {
-                if (priority == Log.ERROR) {
-                    FakeCrashLibrary.logError(t)
-                } else if (priority == Log.WARN) {
-                    FakeCrashLibrary.logWarning(t)
-                }
-            }
         }
     }
 }
