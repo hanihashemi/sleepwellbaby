787fd56de7ae2905ef22c65ae9db1fc697642d65
hani <jhanihashemi@gmail.com>
2018-05-19 00:32:25 +0430

    Working on MainFragmentTest

---
+++ app/src/test/java/com/hanihashemi/sleepwellbaby/ui/main/MainFragmentTest.kt
@@ -0,0 +1,28 @@
+package com.hanihashemi.sleepwellbaby.ui.main
+
+import com.hanihashemi.sleepwellbaby.base.BaseActivityWithSingleFragment
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.spy
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MainFragmentTest {
+    @Test
+    fun updateVoiceFiles() {
+        val activity = startActivity()
+
+        val fragment = spy(activity.fragment as MainFragment)
+
+        assertEquals(21, fragment.musicManager.size)
+
+        fragment.updateVoiceFiles()
+    }
+
+    private fun startActivity(): BaseActivityWithSingleFragment {
+        return Robolectric.buildActivity(MainActivity::class.java)
+                .create().start().get()
+    }
+}
